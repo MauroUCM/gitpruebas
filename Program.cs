881e6859ca18b7f758739e5301856746e8f3abd4
maumar03 <maumar03@ucm.es>
2023-03-13 12:37:34 +0100

    Tipo 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.SetCursorPosition(20, 20);
+            Console.WriteLine("libuzsddlkjblbldfbjlkjdf");
         }
     }
 }
